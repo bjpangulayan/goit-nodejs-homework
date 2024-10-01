14678efeb7c5057fe1a7403379efe2e0b58a886f
bjpangulayan <bjspangulayan23@gmail.com>
2024-10-01 19:43:11 +0800

    Final Commit

---
+++ routes/api/contactsRouter.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
+// prettier-ignore
+import { addContact, deleteContactById, getAllContacts, getContactById, updateContactById, updateStatusContact } from "../../controllers/contactsController.js";
+import { authenticateToken } from "../../middlewares/authenticateToken.js";
+
+const router = express.Router();
+
+/* GET: // http://localhost:3000/api/contacts */
+router.get("/", authenticateToken, ctrlWrapper(getAllContacts));
+
+/* GET: // http://localhost:3000/api/contacts/:contactId */
+router.get("/:contactId", authenticateToken, ctrlWrapper(getContactById));
+
+/* POST: // http://localhost:3000/api/contacts/ 
+*/
+router.post("/", authenticateToken, ctrlWrapper(addContact));
+
+/* DELETE: // http://localhost:3000/api/contacts/:contactId */
+router.delete("/:contactId", authenticateToken, ctrlWrapper(deleteContactById));
+
+/* PUT: // http://localhost:3000/api/contacts/:contactId 
+*/
+router.put("/:contactId", authenticateToken, ctrlWrapper(updateContactById));
+
+/* PATCH: // http://localhost:3000/api/contacts/:contactId/favorite
+*/
+// prettier-ignore
+router.patch("/:contactId/favorite", authenticateToken, ctrlWrapper(updateStatusContact));
+
+export { router };
